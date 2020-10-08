209e258a47c45a11ae4289640e024e2646ce7c3d
bclarke329 <bmendoza329@hotmail.com>
2020-10-08 13:25:52 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog
+
+    def name=(name)
+        @name = name
+    end 
+
+    def name 
+        @name = "Fido"
+    end 
+
+    def breed=(breed)
+        @breed = "Beagle"
+    end 
+
+    def breed
+        @breed
+    end 
+end 
